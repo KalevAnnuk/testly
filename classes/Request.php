de0692a549a7284d3ff67545835373e279ee22e2
Kalev Annuk <kalev.annuk@khk.ee>
2013-04-15 12:58:47 +0300

    pay no attention to the man behind the curtain

---
+++ classes/Request.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Kalev
+ * Date: 15.04.13
+ * Time: 12:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Request {
+	public $controller = DEFAULT_CONTROLLER;
+	public  $action = 'index';
+	public  $params = array();
+}
