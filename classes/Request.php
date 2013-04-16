c93be2a2399e955222d1df98d46190a07e890b5c
Kalev Annuk <kalev.annuk@khk.ee>
2013-04-16 13:02:42 +0300

    Added a lot of things

--- classes/Request.php
+++ classes/Request.php
@@ -1,15 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Kalev
- * Date: 15.04.13
- * Time: 12:54
- * To change this template use File | Settings | File Templates.
- */
-
 class Request
 {
-
 	public $controller;
 	public $action = 'index';
 	public $params = array();
@@ -24,13 +15,10 @@ class Request
 				$this->controller = isset ($path_info[0]) ? array_shift($path_info[0]) : 'welcome';
 				$this->action = isset ($path_info[0]) && ! empty($path_info[0]) ? array_shift($path_info) : 'index';
 				$this->params = isset ($path_info[0]) ? $path_info : NULL;
-
-
 			}
 		}
 	}
 }
-
 $request = new Request;
 print_r($request);
 die();
