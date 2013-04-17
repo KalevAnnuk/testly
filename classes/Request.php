01028ed87a7736420e59b50b334a29aff934eaf9
Kalev Annuk <kalev.annuk@khk.ee>
2013-04-17 11:38:42 +0300

    Add default controller. Add redirect function.

--- classes/Request.php
+++ classes/Request.php
@@ -1,22 +1,28 @@
 <?php
 class Request
 {
-	public $controller;
+
+	public $controller=DEFAULT_CONTROLLER;
 	public $action = 'index';
 	public $params = array();
-
 
 	public function __construct()
 	{
 		if (isset($_SERVER['PATH_INFO'])) {
 			if ($path_info = explode('/', $_SERVER['PATH_INFO'])) {
 				array_shift($path_info);
-				//kill first member of exploded array (because its empty '/')
 				$this->controller = isset ($path_info[0]) ? array_shift($path_info) : 'welcome';
+				var_dump($this->controller);
 				$this->action = isset ($path_info[0]) && ! empty($path_info[0]) ? array_shift($path_info) : 'index';
 				$this->params = isset ($path_info[0]) ? $path_info : NULL;
 			}
 		}
 	}
+
+	public function redirect($destionation)
+	{
+		header('Location: '.BASE_URL.$destionation);
+	}
 }
+
 $request = new Request;
