48eff3a7c154093ed34e7683b24157a33d847fdc
Kalev Annuk <kalev.annuk@khk.ee>
2013-04-19 16:38:02 +0300

    Create function edit to tests controller. Change javascript and css links - replace assets/ with ASSETS_URL. Create test_edit_view.

--- controllers/tests.php
+++ controllers/tests.php
@@ -7,7 +7,7 @@ class tests
 
 	function index()
 	{
-		$this->scripts[]='tests.js';
+		$this->scripts[] = 'tests.js';
 		global $request;
 		global $_user;
 		$tests = get_all("SELECT * FROM test NATURAL JOIN user WHERE test.deleted=0");
@@ -15,10 +15,17 @@ class tests
 		require 'views/master_view.php';
 	}
 
-	function remove(){
+	function remove()
+	{
 		global $request;
-		$id=$request->params[0];
-		$result=q("UPDATE test SET deleted=1 WHERE test_id='$id'");
+		$id = $request->params[0];
+		$result = q("UPDATE test SET deleted=1 WHERE test_id='$id'");
+		require 'views/master_view.php';
+	}
+
+	function edit()
+	{
+		global $request;
 		require 'views/master_view.php';
 	}
 }
--- views/tests_index_view.php
+++ views/tests_index_view.php
@@ -1,7 +1,7 @@
 <p>
 	<a class="btn btn-large btn-info" href="http://google.com">Lisa uus test</a>
 </p>
-<table id="tests-table" class= "table table-bordered table-striped">
+<table id="tests-table" class="table table-bordered table-striped">
 	<thead>
 	<th>Testi nimi</th>
 	<th>Koostaja</th>
@@ -9,16 +9,18 @@
 	<th>Tegevused</th>
 	</thead>
 	<tbody>
-	<? if(! empty($tests)): foreach($tests as $test): ?>
-		<tr id="test<?=$test['test_id'] ?>">
+	<? if (! empty($tests)): foreach ($tests as $test): ?>
+		<tr id="test<?= $test['test_id'] ?>">
 			<td><?=$test['name']?></td>
 			<td><?=$test['username']?></td>
-			<td><?=substr($test['date'],0,10)?></td>
+			<td><?=substr($test['date'], 0, 10)?></td>
 			<td>
-				<i class= "icon-pencil"></i>
+				<a href="<?=BASE_URL?>tests/edit/<?= $test['test_id'] ?>">
+					<i class="icon-pencil"></i>
+				</a>
 				<a href="#" onclick="if(!confirm('Oled kindel?')) return false;
-				remove_test_ajax(<?=$test['test_id']?>); return false">
-				<i class= "icon-trash"></i>Kustuta</a>
+					remove_test_ajax(<?= $test['test_id'] ?>); return false">
+					<i class="icon-trash"></i>Kustuta</a>
 			</td>
 		</tr>
 	<? endforeach; endif ?>
